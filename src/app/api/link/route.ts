80ff9fa1e0610fc42d1f55f1b3dc35ae9674ede9
Jonatandlr <A01735993@tec.mx>
2024-06-22 12:21:46 -0600

    feat: Add API route for creating links

---
+++ src/app/api/link/route.ts
@@ -0,0 +1,65 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/libs/prisma";
+import { getServerSession } from "next-auth";
+
+//post link to api
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const session = await getServerSession();
+    const hash = await hashToUrl(data.hashToUrl);
+
+    if (hash === "Hash already exist") {
+      return NextResponse.json(
+        { message: "Hash already exist" },
+        { status: 400, statusText: "Bad Request" }
+      );
+    }
+
+    //create link in db
+    await db.links.create({
+      data: {
+        URL: data.url,
+        hash: hash,
+        author: {
+          connect: {
+            email: session?.user?.email ?? "",
+          },
+        },
+      },
+    });
+
+
+    // joint the hash to the url
+    const finalLink = `http://localhost:3000/${hash}`;
+
+    return NextResponse.json(
+      { message: "success",finalLink },
+      { status: 200, statusText: "OK" }
+    );
+  } catch (error: any) {
+    console.error("Error al procesar la solicitud:", error);
+    return NextResponse.json(
+      { message: "Error al procesar la solicitud", error: error.message },
+      { status: 500 }
+    );
+  }
+}
+
+async function hashToUrl(hash: string) {
+  //convert white space of hash to hyphen and delete final hyphen
+  hash = hash.replace(/\s+/g, "-").replace(/-$/, "").trim();
+
+  //check if hash exist in db
+  const exist = await db.links.findUnique({
+    where: {
+      hash: hash,
+    },
+  });
+
+  if (exist) {
+    //if exist, return error
+    return "Hash already exist";
+  }
+  return hash;
+}
--- src/components/FormToURL.tsx
+++ src/components/FormToURL.tsx
@@ -8,9 +8,19 @@ const FormToURL = () => {
     url: "",
     hashToUrl: "",
   });
-
   const [crearLink, setCrearLink] = useState(false);
-  const { data } = useSession();
+  const [loading, setLoading] = useState(false);
+  const [finalLink, setFinalLink] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(finalLink).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1000);
+    });
+  };
+
+  const { data: sesion } = useSession();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -19,14 +29,26 @@ const FormToURL = () => {
     });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!data) {
+    if (!sesion) {
       setCrearLink(true);
+      return;
     }
 
-    
+    setLoading(true);
+    const response = await fetch("http://localhost:3000/api/link", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    setFinalLink(data.finalLink);
+    setLoading(false);
+    console.log(data.finalLink);
   };
 
   return (
@@ -36,7 +58,7 @@ const FormToURL = () => {
           id="url"
           name="url"
           type="url"
-          className=" rounded-[15px] border-2 border-black/opacity-50 px-4 py-2 w-[550px] focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent"
+          className=" rounded-[15px] border-2 border-black/opacity-50 px-4 py-2 md:w-[550px] w-full focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent"
           placeholder="Ingresa URL"
           onChange={handleChange}
           value={formData.url}
@@ -54,30 +76,99 @@ const FormToURL = () => {
             maxLength={10}
             required
           />
-          <Button button="red" >Cortar &rarr;</Button>
+          <Button button="red">Cortar &rarr;</Button>
         </div>
       </form>
+
       {crearLink ? (
         <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center">
-          <div 
-          onClick={()=>{setCrearLink(false)}}
-          
-          className="absolute top-0 left-0 h-screen w-screen bg-black opacity-80"></div>
+          <div
+            onClick={() => {
+              setCrearLink(false);
+            }}
+            className="absolute top-0 left-0 h-screen w-screen bg-black opacity-80"
+          ></div>
 
           <div className="relative bg-white z-10 py-7 md:px-8 px-4 rounded-2xl my-2">
             <h3 className="md:text-lg text-base text-center pb-4 font-semibold">
               Necesitas iniciar sesión para crear un link
             </h3>
             <div className="flex justify-end">
-              <Button click={()=>{setCrearLink(false)}} button="principal">cerrar</Button>
+              <Button
+                click={() => {
+                  setCrearLink(false);
+                }}
+                button="principal"
+              >
+                cerrar
+              </Button>
             </div>
           </div>
         </div>
       ) : (
         <div></div>
       )}
-
-
+      {loading ? (
+        <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center">
+          <div className="absolute top-0 left-0 h-screen w-screen bg-black opacity-80"></div>
+          <div className="relative bg-white z-10 py-7 md:px-8 px-4 rounded-2xl my-2">
+            <h3 className="md:text-lg text-base text-center pb-4 font-semibold">
+              Creando link...
+            </h3>
+          </div>
+        </div>
+      ) : (
+        <div>
+          {finalLink ? (
+            <div className="flex flex-col  pt-8">
+              <div className="relative flex items-center rounded-[15px] border-2 border-black/opacity-50 px-4 py-2 md:w-[550px] w-full focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent">
+                <input
+                  className="flex-1 bg-transparent outline-none"
+                  value={finalLink}
+                  readOnly
+                />
+                <button
+                  onClick={handleCopy}
+                  className="ml-2 p-1 text-black hover:text-red-600 focus:outline-none"
+                  aria-label="Copy to clipboard"
+                >
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-copy"
+                  >
+                    <rect
+                      x="9"
+                      y="9"
+                      width="13"
+                      height="13"
+                      rx="2"
+                      ry="2"
+                    ></rect>
+                    <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+                  </svg>
+                </button>
+                {copied && (
+                  <span className="absolute right-7 top-0 mt-2 mr-2 text-green-600">
+                    Copied!
+                  </span>
+                )}
+              </div>
+              <h3 className="md:w-[550px] w-full text-xl font-semibold  text-rose-600 text-center">
+                Aqui esta tu link!
+              </h3>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
